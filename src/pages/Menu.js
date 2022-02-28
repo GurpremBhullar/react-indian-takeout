102484dd3fb2957d5cfcc17f88c929bbc99b7e34
Gurprem Bhullar <bhullargurprem@gmail.com>
2022-02-27 23:03:56 -0500

    completed the menu

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import { Data } from '../data/Data.js';
+import MenuItem from '../components/MenuItem.js';
+import '../styles/Menu.css';
 
 function Menu() {
-  return (
-    <div className="menu">
-        <div className="menuTitle">
-            <div className="menuList">
-                {Data.map((menuItem, key) => {
-                    return <div>{menuItem.price}</div>;
+    return (
+        <div className="menu">
+            <div className="menuTitle">
+                <div className="menuList">
+                    {Data.map((menuItem, key) => {
+                        return (
+                            <MenuItem
+                                key={key}
+                                image={menuItem.image}
+                                name={menuItem.name}
+                                price={menuItem.price} />
+                                );
                     })}
+                </div>
             </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default Menu;
